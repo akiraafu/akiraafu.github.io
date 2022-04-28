5f195c21475f9564da2fdff02edbb7b1e09d2505
Junrong <87742302+akiraafu@users.noreply.github.com>
2022-04-28 22:13:19 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,17 @@
+document.getElementById("resultCard").style.display = "none";
+document.getElementById("rateCard").style.display = "gird";
+
+const resultDisplay = document.getElementById("result");
 const ratings = document.querySelectorAll(".rate");
+let result;
+
+ratings.forEach((rate) =>
+    rate.addEventListener("click", (e) => {
+        rate = e.target.id;
+        resultDisplay.innerHTML = rate;
+    })
+);
+
 ratings.forEach((rate) => {
     rate.addEventListener("click", function () {
         rate.style.backgroundColor = "hsl(25, 97%, 53%)";
@@ -7,26 +20,10 @@ ratings.forEach((rate) => {
 
 const button = document.querySelector(".btn");
 button.addEventListener("click", function () {
-    let contentP = document.getElementById("text");
-    contentP.innerHTML =
-        "We appreciate you taking the time to give a rating. If you ever need more support, don’t hesitate to get in touch!";
-    let h1 = document.querySelector("h1");
-    h1.innerHTML = "Thank you!";
+    let rateCard = document.getElementById("rateCard");
+    rateCard.classList.toggle("rateCard");
+    while (rateCard.firstChild) {
+        rateCard.removeChild(rateCard.firstChild);
+    }
+    document.getElementById("resultCard").style.display = "grid";
 });
-
-// button.addEventListener("click", function() {
-//     const newContainer = document.createElement("div");
-//     newContainer.classList.add("container");
-//     const h3 = document.createElement("h3");
-//     h3.textContent = "You selected out of 5";
-//     const h2 = document.createElement("h2");
-//     h2.textContent = "Thank you!";
-//     const p = document.createElement("p");
-//     p.classList.add("thankYou");
-//     p.textContent =
-//         "We appreciate you taking the time to give a rating. If you ever need more support, don’t hesitate to get in touch!";
-//     document.body.appendChild(newContainer);
-//     newContainer.appendChild(h3);
-//     newContainer.appendChild(h2);
-//     newContainer.appendChild(p);
-// })
